e2814b4a7fe098894ecdfdd7cace0c0bd1298604
patrickchow0803 <patrickchow0803@gmail.com>
2021-06-15 17:28:36 -0400

    added movie_bloc

---
+++ lib/bloc/movie_bloc/movie_bloc.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_viewer/infrastructure/api_service.dart';
+import 'package:movie_viewer/models/movie.dart';
+
+import 'movie_bloc_event.dart';
+import 'movie_bloc_state.dart';
+
+class MovieBloc extends Bloc<MovieEvent, MovieState> {
+  MovieBloc() : super(MovieLoading());
+
+  @override
+  Stream<MovieState> mapEventToState(MovieEvent event) async* {
+    if (event is MovieEventStarted) {
+      yield* _mapMovieEventStateToState(event.movieId, event.query);
+    }
+  }
+
+  Stream<MovieState> _mapMovieEventStateToState(
+      int movieId, String query) async* {
+    final service = ApiService();
+    // initial state
+    yield MovieLoading();
+    try {
+      late List<Movie> movieList;
+      if (movieId == 0) {
+        movieList = await service.getNowPlayingMovie();
+      }
+
+      yield MovieLoaded(movieList);
+    } on Exception catch (e) {
+      print(e);
+      yield MovieError();
+    }
+  }
+}
