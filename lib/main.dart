c62d16bc8bb037a2a862a3945f0017f6d6406b3a
patrickchow0803 <patrickchow0803@gmail.com>
2021-06-15 17:02:30 -0400

    added dotenv to hide api key and implemented the getNowPlayingMovie method

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_viewer/presentation/home_screen.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
